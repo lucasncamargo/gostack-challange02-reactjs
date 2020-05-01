311cbc3ed503b3281a4d19ca93e1004f1d6a24aa
Lucas Camargo <lucasn.camargo@gmail.com>
2020-04-30 22:58:24 -0300

    App com todas as funcionalidades criadas e validadas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then((res) => {
+      setRepositories(res.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const res = await api.post('repositories', {
+        url: "https://github.com/lucasncamargo",
+        title: "Challange 02 - React.js",
+        techs: ["React", "Node.js"],
+      });
+      setRepositories([...repositories, res.data]);
+    } catch {
+      alert('Não foi possível adicionar o repositório...');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+      setRepositories(repositories.filter((repo) => repo.id !== id));
+    } catch {
+      alert('Não foi possível excluir o repositório...');
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
